0ad6bf59e6d003a6332004bdf6724b2ac4737f24
richa1999 <64058980+richa1999@users.noreply.github.com>
2023-09-17 19:18:26 +0530

    React elements without JSX

---
+++ Basics_of_react/App.js
@@ -0,0 +1,26 @@
+// ResctElement creation without using JSX.
+const heading = React.createElement(
+	"h1",
+	{
+		id: "heading",
+	},
+	"Hello World from React"
+);
+
+// nested React.createElement
+const parent = React.createElement("div", { id: "parent" }, [
+	React.createElement("div", { id: "child" }, [
+		// child passed as third param.
+		React.createElement("h1", {}, "I'm h1 tag"), // siblings passed in array
+		React.createElement("h2", {}, "I'm h2 tag"),
+	]),
+	React.createElement("div", { id: "child2" }, [
+		React.createElement("h1", {}, "I'm h1 tag"),
+		React.createElement("h2", {}, "I'm h2 tag"),
+	]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading);
+console.log(parent);
+root.render(parent);
