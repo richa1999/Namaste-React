3db533ff010a2a2984f841c383d08142cc1cd604
richa1999 <64058980+richa1999@users.noreply.github.com>
2024-04-17 08:45:12 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,29 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// ResctElement creation without using JSX.
+//REct.createElement => returns objext => HTMLElement(render)
+
 const heading = React.createElement(
 	"h1",
-	{
-		id: "heading",
-	},
-	"Hello World from React"
+	{ id: "heading" },
+	"Namaste JAvascript"
 );
 
-// nested React.createElement
-const parent = React.createElement("div", { id: "parent" }, [
-	React.createElement("div", { id: "child" }, [
-		// child passed as third param.
-		React.createElement("h1", {}, "I'm h1 tag"), // siblings passed in array
-		React.createElement("h2", {}, "I'm h2 tag"),
-	]),
-	React.createElement("div", { id: "child2" }, [
-		React.createElement("h1", {}, "I'm h1 tag"),
-		React.createElement("h2", {}, "I'm h2 tag"),
-	]),
-]);
+console.log(heading);
+
+// JSX transpiled before it goes to JS engine. - parcel - package babel.
+
+// JSX => React.createElement => object => HTML
+
+const jsxHeading = <h1 id="heading"> Namaste React </h1>;
+
+console.log(jsxHeading);
+
+const Title = () => (
+	<h1 id="heading"> Namaste from concise functional component </h1>
+);
+
+// Component Composition => putting one component in another component.
+const HeadingComponent = () => {
+	return (
+		<>
+			<Title />
+			<h1 id="heading"> Namaste from functional component </h1>
+		</>
+	);
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(heading);
-console.log(parent);
-root.render(parent);
+
+root.render(<HeadingComponent />);
