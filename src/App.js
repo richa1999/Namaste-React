62e2ab058b679ccd80db53ed7fffcde9e739efad
richa1999 <64058980+richa1999@users.noreply.github.com>
2024-05-06 09:16:28 +0530

    shimmer-ui and fetch api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import restaurants from "./utils/mockData";
 
 const AppLayout = () => {
-	const [topRestaurants, setTopRestaurants] = useState(restaurants);
+	const [topRestaurants, setTopRestaurants] = useState([]);
 	const onClickHandler = () => {
 		const topRestaurants = restaurants.filter((restaurant) => restaurant.info.avgRating > 4);
 		setTopRestaurants(topRestaurants);
+	}
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const fetchData = async () => {
+		const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6460176&lng=77.3695166&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+		const data = await response.json();
+		console.log(data);
+		setTopRestaurants(data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants); 
 	}
 	return <div className="app">
 		<Header />
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import RestaurantCard from './RestaurantCard';
+import Shimmer from './Shimmer';
 
 const Body = (props) => {
     const { restaurants, onClickHandler } = props;
+    if(restaurants.length === 0) {
+        return <Shimmer />;
+    }
     return (
         <div className="body">
             <div className="search">Search</div>
