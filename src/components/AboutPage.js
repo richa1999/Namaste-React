0296dab7a56ba27afce2d32cd86ec90320dfac7a
richa1999 <64058980+richa1999@users.noreply.github.com>
2024-05-07 09:20:18 +0530

    class component

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,9 +1,23 @@
-const AboutPage = () => {
-  return (
-    <div>
+import React from "react";
+import UserClass from "./UserClass";
+
+class AboutPage extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent ComponentDidMount");
+  }
+
+  render() {
+    console.log("Parent Render");
+    return <div>
       <h1>About</h1>
+      <UserClass name={"Richa"} location={"Ghaziabad"} contact={"@richasr07"}/>
     </div>
-  );
+  }
 };
 
 export default AboutPage;
---
+++ src/components/UserClass.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+    // console.log(this.props.name + "Child Constructor");
+  }
+
+  componentDidMount() {
+    // console.log(this.props.name + "Child ComponentDidMount");
+  }
+  render() {
+    // console.log(this.props.name + "Child Render");
+    const { name, location, contact } = this.props;
+    // never update the state variable directly
+    // const { count, count1 } = this.state;
+    return (
+      <div className="userCard">
+        <h1>{name}</h1>
+        <h2>{location}</h2>
+        <h3>{contact}</h3>
+      </div>
+    );
+  }
+}
+
+/* 
+
+Parent Constructor
+Parent Render
+
+First Child Constructor
+First Child Render
+
+Second Child Constructor
+Second Child Render
+
+<DOM UPDATED - IN SINGLE BATCH>
+
+First Child ComponentDidMount
+Second Child ComponentDidMount
+
+Parent ComponentDidMount
+
+*/
+
+export default UserClass;
